f12dd6ef046f193b53a9ade86b41accf24e8e7e8
JENNIFER WADELLA <jennifer.wadella@gmail.com>
2015-12-02 19:50:04 -0600

    adding env port cause I am dumb

--- src/server/index.js
+++ src/server/index.js
@@ -2,11 +2,12 @@ var express = require('express');
 var app = express();
 var favicon = require('serve-favicon');
 
+var port = process.env.PORT || 3000;
 export default {
    start: (dir) => {
       //app.use(favicon(dir + '/public/favicon.ico'));
       app.use('/assets', express.static('./public/assets'));
-      var server = app.listen(3000, function () {
+      var server = app.listen(port, function () {
          var host = server.address().address;
          var port = server.address().port;
       });
