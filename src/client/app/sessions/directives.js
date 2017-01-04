0b06c56a4493aca6ce0d3d8e7b0ee1451943bca0
Lyndsey Padget <lpadget@freightview.com>
2017-01-03 23:59:01 -0600

    Add the list to the sessions page

--- src/client/app/sessions/directives.js
+++ src/client/app/sessions/directives.js
@@ -3,6 +3,12 @@ function sessionnav() {
       template: require('./partials/sessionnav.html')
    }
 }
+function sessionlist() {
+   return {
+      template: require('./partials/sessionlist.html')
+   }
+}
 
 export default angular.module('sessions.directives', [])
    .directive('sessionnav', sessionnav)
+   .directive('sessionlist', sessionlist)
