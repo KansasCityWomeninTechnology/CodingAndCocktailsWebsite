442abec67d69546cce74cf4be6e4a225a46160f8
Lyndsey Padget <lpadget@freightview.com>
2017-01-04 11:26:45 -0600

    Final updates, I think

--- src/client/app/sessions/controllers.js
+++ src/client/app/sessions/controllers.js
@@ -1,18 +1,3 @@
 'use strict';
 
-function sessionsCtrl() {
-   let svm = this;
-   svm.sessions = [{
-      title: 'Coding & Cocktails - The Tools',
-      description: 'This is a reference for the developer tools we will be using in class and how to configure them.',
-      id: 'tools',
-      links: [{
-         button: 'Download Worksheet',
-         url: require('../../worksheets/TheTools.pdf')
-      }]
-      }
-   ]
-}
-
 export default angular.module('sessions.controllers', [])
-   .controller('sessionsCtrl', sessionsCtrl);
--- src/client/app/sessions/directives.js
+++ src/client/app/sessions/directives.js
@@ -8,7 +8,13 @@ function sessionlist() {
       template: require('./partials/sessionlist.html')
    }
 }
+function tools() {
+   return {
+      template: require('./partials/tools.html')
+   }
+}
 
 export default angular.module('sessions.directives', [])
    .directive('sessionnav', sessionnav)
    .directive('sessionlist', sessionlist)
+   .directive('tools', tools)
